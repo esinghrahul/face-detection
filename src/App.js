a3639d32129feb40fd97f25bd7c3af8a426d7b80
esinghrahul <e.singh.rahul@outlook.com>
2020-07-03 12:54:18 +0530

    Added components and styling changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Particles from "react-particles-js";
+import "./App.css";
+import Navigation from "./components/navigation/navigation.component";
+import Logo from "./components/logo/logo.component";
+import ImageForm from "./components/image-form/image-form.component";
+import Rank from "./components/rank/rank.component";
 
-function App() {
+const PARTICLE_OPTIONS = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 700,
+      },
+    },
+  },
+};
+class App extends React.Component {
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className= 'particles' params = {PARTICLE_OPTIONS} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageForm />
     </div>
-  );
+  );}
 }
 
 export default App;
