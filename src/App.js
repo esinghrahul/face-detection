6622fe280aa8f4da2ea38705eefe0f6e22bc039f
esinghrahul <e.singh.rahul@outlook.com>
2020-07-03 14:31:51 +0530

    Added functions to handle face detection and relevant styling for highlighting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Particles from "react-particles-js";
+import Clarifai from 'clarifai'
 import "./App.css";
 import Navigation from "./components/navigation/navigation.component";
 import Logo from "./components/logo/logo.component";
 import ImageForm from "./components/image-form/image-form.component";
 import Rank from "./components/rank/rank.component";
+import FaceDetection from "./components/face-detection/face-detection.component";
+
+const app = new Clarifai.App({
+  apiKey: 'ac0a06c8a8774551a61fd675c50137cb'
+ });
 
 const PARTICLE_OPTIONS = {
   particles: {
@@ -18,6 +24,41 @@ const PARTICLE_OPTIONS = {
   },
 };
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      input: '',
+      imageUrl: '',
+      box: {}
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputImage')
+    const width = Number(image.width)
+    const height = Number(image.height)
+    return{
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box})
+  }
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models.predict('e15d0f873e66047e579f90cf82c9882z', this.state.input)
+    .then((response) =>  this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err))
+  }
   render(){
   return (
     <div className="App">
@@ -25,7 +66,8 @@ class App extends React.Component {
       <Navigation />
       <Logo />
       <Rank />
-      <ImageForm />
+      <ImageForm onSubmit = {this.onSubmit} onInputChange = {this.onInputChange} />
+      <FaceDetection imageUrl = {this.state.imageUrl} box = {this.state.box}/>
     </div>
   );}
 }
